d2a8b343ba5d820f902c75b4258c3c900fe57c92
ArduinoHocam <ArduinoHocam@users.noreply.github.com>
2018-01-06 17:32:23 +0300

    KODLAR(CODES)
    KODLAR(CODES)

---
+++ proteus_simulation.ino
@@ -0,0 +1,32 @@
+          
+            /*ARDUINO HOCAM PROTEUS SIMULASYON*/
+
+
+void setup() {
+
+pinMode(2,OUTPUT);
+pinMode(3,OUTPUT);
+pinMode(4,OUTPUT);
+pinMode(5,OUTPUT);
+}
+
+void loop() {
+  
+digitalWrite(2,1);// digital pin2 HIGH
+delay(50);
+digitalWrite(3,1);
+delay(50);
+digitalWrite(4,1);
+delay(50);
+digitalWrite(5,1);
+delay(50);
+digitalWrite(5,0);
+delay(50);
+digitalWrite(4,0);
+delay(50);
+digitalWrite(3,0);
+delay(50);
+digitalWrite(2,0);
+delay(50);
+
+}
